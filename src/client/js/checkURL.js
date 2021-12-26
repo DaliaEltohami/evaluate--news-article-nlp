261ca7e12ea70b48521e0e33e7816a4866326837
DaliaEltohami <daliaeltohami1991@gmail.com>
2021-12-26 17:23:11 +0200

    define the checkForURL function which contains a regexp to check for the url validation

--- src/client/js/checkURL.js
+++ src/client/js/checkURL.js
@@ -1,7 +1,16 @@
 function checkForURL(url) {
-    console.log("::: Running checkForName :::", url);
-
-    return true
+    console.log("::: Running checkForURL :::", url);
+    // define regex to check for url validation
+    const regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    // return ture if the url is valid and false otherwise
+    if (regexp.test(url))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
 
 }
 
