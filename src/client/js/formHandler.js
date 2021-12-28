ce9bdce9b3c21972b247b047650ac24769568d1b
DaliaEltohami <daliaeltohami1991@gmail.com>
2021-12-28 10:18:47 +0200

    add needed setup to server index.js file, define post route 'call-api', create the .env file to hold the environment variables

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,6 +1,6 @@
 // handler that will run when form submit event occure
 
-const handleSubmit = (event) => {
+const handleSubmit = async(event) => {
     // prevent default action of submit event
     event.preventDefault()
     // get the url text that the user input
@@ -9,7 +9,32 @@ const handleSubmit = (event) => {
     const valid = Client.checkForURL(URL)
     // check for tha value of valid if true post the url to the server route else show alert to user 
     if(valid){
-        alert("ok")
+        // post the url address to the server route that will call the meaningCloud API
+        try{
+            // make post request
+            const response = await fetch('http://localhost:8081/call-api',{
+                method : 'post',
+                credentials : 'same-origin',
+                mode: 'cors',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({url : URL})
+            });
+            // receive the returned data from the server and convert it to json
+            const data = await response.json()
+            // uodate the view with the returned data by setting the innerHTML property of the place holders in DOM
+            document.getElementById('result1').innerHTML = `<p>text : ${data.text}</p>`;
+            document.getElementById('result2').innerHTML = `<p>score tag : ${data.score_tag}</p>`;
+            document.getElementById('result3').innerHTML = `<p>subjectivity : ${data.subjectivity}</p>`;
+            document.getElementById('result4').innerHTML = `<p>confidence : ${data.confidence}</p>`;
+            document.getElementById('result5').innerHTML = `<p>agreement : ${data.agreement}</p>`;
+            document.getElementById('result6').innerHTML = `<p>irony : ${data.irony}</p>`;
+
+        }
+        catch(error){
+            console.log('error',error);
+        }
     }else{
         alert("Please Enter Valid URL!!")
     }
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,29 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const bodyParser = require('body-parser');
+const cors = require("cors");
+// import axios module to make API get request
+const axios = require("axios");
 
 const app = express()
-
+// setup cors options
+const corsOptions = {
+    origin:'*', 
+    credentials:true,            
+    optionSuccessStatus:200,
+ }
+ 
+ // specify the static folder to start the app from
 app.use(express.static('dist'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors(corsOptions)) 
+
+// require dotenv to access the environment variables
+const dotenv = require('dotenv');
+const { response } = require('express');
+dotenv.config();
 
 console.log(__dirname)
 
@@ -20,4 +39,44 @@ app.listen(8081, function () {
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+// create the post route /call-api
+app.post('/call-api', async(req,res)=>{
+    try{
+        // Define variable for baseURL of the webAPI
+        const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=';
+        // Define variable for API key
+        const personalAPIKey = process.env.API_KEY;
+        // console.log(req.body)
+        // Define variable to get the element that will have the value of article url
+        const url = req.body.url
+        // form the full api url from baseURL, personalAPIKey and given url form the user
+        const APIFullURL = baseURL+personalAPIKey+'&url='+url+'&lang=en'
+
+        // make get request using axios
+        axios.get(APIFullURL)
+        // get the response from the request and form the needed data be send back to the app
+        .then(response =>{
+            // form the object of the needed data
+            console.log(response)
+            const returnedData = {
+                text: response.data.sentence_list[0].text,
+                score_tag: response.data.sentence_list[0].score_tag,
+                agreement: response.data.sentence_list[0].agreement,
+                subjectivity: response.data.subjectivity,
+                confidence: response.data.sentence_list[0].confidence,
+                irony: response.data.irony
+            }
+            // send the response back to the server with the created object
+            res.send(returnedData)
+        })
+        // catch if there is any errors
+        .catch(error => {
+            console.log('Error: '+error)
+        })
+    }
+    catch(error){
+        console.log('Error: '+error)
+    }
 })
